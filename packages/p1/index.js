1e61b12ae8540a15615a7963a8360764ec0c50eb
Max Donchenko <maxx.donchenko@gmail.com>
2020-03-26 18:04:53 +0200

    add 3rd party dependency to the root dependency (no lerna hoisting) lerna performs symlinking under the hood so node_modules aren't get copied each time

--- packages/p1/index.js
+++ packages/p1/index.js
@@ -1,9 +1,13 @@
+const math = require('mathjs');
+
 function plus1(x) {
   return x + 1;
 }
 
 function multiplyBy2(x) {
-  return x * 2;
+  return math.chain(x)
+    .multiply(2)
+    .done();
 }
 
 module.exports = {
