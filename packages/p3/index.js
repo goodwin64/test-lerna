49a7e321e0438ce60fd41dbc79fce12bab73e6a3
Max Donchenko <maxx.donchenko@gmail.com>
2020-03-26 17:55:35 +0200

    installed all dependencies via "lerna add" instead of manually

--- packages/p3/index.js
+++ packages/p3/index.js
@@ -1,5 +1,8 @@
 const { plus2 } = require('@namespace/p2');
+const { multiplyBy2 } = require('@namespace/p1');
 
-const result = plus2(5);
-console.log('result:', result); // 7
+const result5plus1 = plus2(5);
+const result10times2 = multiplyBy2(10);
 
+console.log('result5plus1:', result5plus1); // 7
+console.log('result10times2:', result10times2); // 20
